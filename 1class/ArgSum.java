35f368defccdc253b07b56af69c5b187f9872cc5
liu <liujiaqier@gmail.com>
2020-03-28 20:29:30 -0700

    modify note

--- 1class/ArgSum.java
+++ 1class/ArgSum.java
@@ -2,8 +2,8 @@ public class ArgSum
 {
   public static void main(String[] args)
   {
-    int summation = 0;
-    for (int i = 0; i < args.length; i++)
+    int summation=0;
+    for (int i=0; i<args.length; i++)
     {
       summation+=Integer.parseInt(args[i]);
     }
