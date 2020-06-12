1ffc8aab156060f9361e4cb2dbca99559fbfe8b0
liu <liujiaqier@gmail.com>
2020-06-11 19:13:46 -0700

    proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,78 +1,48 @@
 public class ArrayDeque<T> {
-    private T[] items;
     private int size;
-    private int nextFirst;
-    private int nextLast;
-    private static final int INIT_CAPACITY = 8;
-    private static final int RFACTOR = 2;
-    private static final double MIN_USAGE_RATIO = 0.25;
+    private int first;
+    private int last;
+    private T[] array;
 
-    /** Creates an empty array deque.
-     *  The starting size of your array should be 8.
-     */
     public ArrayDeque() {
-        items = (T[]) new Object[INIT_CAPACITY];
         size = 0;
-        nextFirst = 4;
-        nextLast = 5;
+        array = (T[]) new Object[8];
+        first = 0;
+        last = 0;
     }
 
-    /** Creates a deep copy of other. */
-    public ArrayDeque(ArrayDeque other) {
-        items = (T[]) new Object[INIT_CAPACITY];
-        size = 0;
-        nextFirst = 4;
-        nextLast = 5;
-        for (int i = 0; i < other.size(); i++) {
-            addLast((T) other.get(i));
-        }
+    private int plusOne(int x) {
+        return (x + 1 - array.length) % array.length;
     }
 
-    private int minusOne(int index) {
-        return (index - 1 + items.length) % items.length;
-    }
-
-    private int plusOne(int index) {
-        return (index + 1) % items.length;
+    private int minusOne(int x) {
+        return (x - 1 + array.length) % array.length;
     }
 
     private void resize(int capacity) {
-        T[] newArray = (T[]) new Object[capacity];
-
-        int curr = plusOne(nextFirst);
-        for (int i = 0; i < size; i++) {
-            newArray[i] = items[curr];
-            curr = plusOne(curr);
-        }
-
-        items = newArray;
-        nextFirst = capacity - 1;
-        nextLast = size;
+        T[] a = (T[]) new Object[capacity];
+        first = capacity - 1;
+        last = size;
     }
 
-    /** Adds an item of type T to the front of the deque. */
     public void addFirst(T item) {
-        if (size == items.length) {
-            resize(size * RFACTOR);
+        array[first] = item;
+        first = plusOne(first);
+        size++;
+        if (size == array.length) {
+            resize(size * 2);
         }
-
-        items[nextFirst] = item;
-        size += 1;
-        nextFirst = minusOne(nextFirst);
     }
 
-    /** Adds an item of type T to the back of the deque. */
     public void addLast(T item) {
-        if (size == items.length) {
-            resize(size * RFACTOR);
+        array[last] = item;
+        last = minusOne(last);
+        size++;
+        if (size == array.length) {
+            resize(size * 2);
         }
-
-        items[nextLast] = item;
-        size += 1;
-        nextLast = plusOne(nextLast);
     }
 
-    /** Returns true if deque is empty, false otherwise. */
     public boolean isEmpty() {
         return size == 0;
     }
@@ -82,58 +52,45 @@ public class ArrayDeque<T> {
     }
 
     public void printDeque() {
-        int i = plusOne(nextFirst);
-
-        while (i != nextLast) {
-            System.out.print(items[i] + " ");
-            i = plusOne(i);
+        int f = plusOne(first);
+        while (f != last) {
+            System.out.print(array[f] + " ");
+            f = plusOne(f);
         }
-        System.out.println();
     }
-
 
     public T removeFirst() {
         if (size == 0) {
             return null;
         }
-
-        int first = plusOne(nextFirst);
-        T firstItem = items[first];
-        items[first] = null;
-        nextFirst = first;
-        size -= 1;
-
-        if (items.length >= 16 && size < items.length * MIN_USAGE_RATIO) {
-            resize(items.length / 2);
+        first = plusOne(first);
+        T res = array[first];
+        size--;
+        if (array.length > 8 && size < array.length/4) {
+            resize(array.length / 2);
         }
-
-        return firstItem;
+        return res;
     }
 
-    public T removeLast() {
+    private T removeLast() {
         if (size == 0) {
             return null;
         }
-
-        int last = minusOne(nextLast);
-        T lastItem = items[last];
-        items[last] = null;
-        nextLast = last;
-        size -= 1;
-
-        if (items.length >= 16 && size < items.length * MIN_USAGE_RATIO) {
-            resize(items.length / 2);
+        last = minusOne(last);
+        T res = array[last];
+        size--;
+        if (array.length > 8 && size < array.length/4) {
+            resize(array.length/2);
         }
-
-        return lastItem;
+        return res;
     }
 
     public T get(int index) {
-        if (index > size) {
+        if (index >= size) {
             return null;
         }
-
-        index = (plusOne(nextFirst) + index) % items.length;
-        return items[index];
+        index = (plusOne(first) + index) % array.length;
+        return array[index];
     }
+
 }
